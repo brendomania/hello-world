85af8c341074445aa3104fa504ca108142b9dd72
brendomania <dancoy2@gmail.com>
2026-02-20 05:24:16 +0100

    small refactor

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World!</p>
-
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noopener noreferrer">
+          <img src={logo} className="App-logo" alt="logo" />
+        </a>
+        <div className="App-text">
+          <p>Hello World!</p>
+          <p>Good morning Frontend Hero!</p>
+        </div>
       </header>
-    </div >
+    </div>
   );
 }
 
